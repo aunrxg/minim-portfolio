96ae651df9d430c89e48c55bb4328445767d3aec
anuraginbytes <anuragpoddar9484@gmail.com>
2025-03-05 19:23:40 +0530

    add readme and assests

---
+++ constants/experience.ts
@@ -0,0 +1,20 @@
+
+export const EXPERIENCES: {
+  company: string;
+  role: string;
+  date: string;
+  logo: string;
+}[] = [
+  {
+    company: "Saral Startup School",
+    role: "Web Developer Intern",
+    date: "December 2024 - Present",
+    logo: "/sss_logo.jpeg"
+  },
+  {
+    company: "Smart Bridge",
+    role: "Full Stack Developer Intern and trainee",
+    date: "August 2024 - October 2024",
+    logo: "/smart-bridge.jpeg"
+  },
+];
---
+++ constants/projects.ts
@@ -0,0 +1,34 @@
+
+export const PROJECTS: {
+  href?: string;
+  title: string;
+  description: string;
+  image: string;
+}[] = [
+  {
+    title: 'Project1',
+    href: 'https://github.com/anuraginbytes',
+    description: 'Description 1',
+    image: '/glow.png',
+  },
+  {
+    title: 'Project2',
+    href: 'https://github.com/anuraginbytes',
+    description:
+      'Description 2',
+    image: '/glow.png',
+  },
+  {
+    title: 'Project3',
+    href: 'https://github.com/anuraginbytes',
+    description:
+      'Description 3',
+    image: '/glow.png',
+  },
+  {
+    href: 'https://github.com/anuraginbytes',
+    title: 'Project4',
+    description: 'Description 4',
+    image: '/glow.png',
+  },
+];

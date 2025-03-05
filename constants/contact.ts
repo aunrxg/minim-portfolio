45e07ecbb0edced219f86577a48e32762c0880cc
anuraginbytes <anuragpoddar9484@gmail.com>
2025-03-05 22:33:44 +0530

    add contact details

---
+++ constants/contact.ts
@@ -0,0 +1,27 @@
+
+export const CONTACT: {
+  method: string;
+  link: string;
+  label: string;
+}[] = [
+  {
+    method: "Email",
+    link: "mailto:anuragpoddar9484@gmail.com",
+    label: "anuragpoddar9484@gmail.com",
+  },
+  {
+    method: "Twitter",
+    link: "https://x.com/aunrxg",
+    label: "@aunrxg",
+  },
+  {
+    method: "GitHub",
+    link: "https://github.com/anuraginbytes",
+    label: "github/anuraginbytes",
+  },
+  {
+    method: "LinkedIn",
+    link: "https://linkedin.com/in/anurag-poddar-dev",
+    label: "in/anurag-poddar-dev",
+  },
+];
